2596a39c418fa8a8c7b4aa9d891c3b35a976d57e
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-21 10:40:04 +0300

    New Project

--- JS1/Program.cs
+++ JS1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+
+
+string[] Array = new string[5];
+
+Console.WriteLine("Заполните массив: ");
+for (int i = 0; i < 5; i++)
+{
+    Array[i] = Console.ReadLine();
+
+}
+
+Console.WriteLine();
+Console.Write("[" + string.Join(",", Array) + "]");
+Console.Write(" -> [ ");
+
+for (int i = 0; i < 5; i++)
+{
+
+    if (Array[i].Length <= 3)
+    {
+        Console.Write($"{Array[i]},");
+    }
+}
+Console.Write("\b]");
