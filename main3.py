20309d36637389069bfcf63ffd522f77e99f1d82
Zeenore <gaidamaka2003oct@gmail.com>
2022-12-27 15:36:18 +0300

    123

--- main3.py
+++ main3.py
@@ -1,10 +1,10 @@
 from random import randint
-numbers = [randint(1, 50) for i in range(10)]
+numbers = [randint(1, 15) for i in range(5)]
 print(numbers)
 sot = numbers.copy()
 print(sot)
 sot = sorted(sot)
 print(sot)
 sot.pop(0)
-sot.pop(-1)
+sot.pop()
 print(sum(sot))
